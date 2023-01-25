2ab43628f20cd82f4fb89442acd7f1d6785cf2f9
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-01-25 00:09:28 -0300

    feat: Add to_be method

--- lib/test_framework.rb
+++ lib/test_framework.rb
@@ -1,21 +1,19 @@
-module TestFramework
-  VERSION = '6.1.0'
+VERSION = '6.1.0'
 
-  class TestFrameworkCore
-    def beforeEach
-      yield
-    end
+class TestFrameworkCore
+  def beforeEach
+    yield
+  end
 
-    def afterEach
-      yield
-    end
+  def afterEach
+    yield
+  end
 
-    def beforeAll
-      yield
-    end
+  def beforeAll
+    yield
+  end
 
-    def afterAll
-      yield
-    end
+  def afterAll
+    yield
   end
 end
