057075fa99f81ae8a6c536d5db5c97fcee550e26
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-01-27 20:58:36 -0300

    Add test_framework method

--- lib/test_framework.rb
+++ lib/test_framework.rb
@@ -1,54 +1,55 @@
-class TestFramework
-  @@total_tests = 0
-  @@before_eachs = []
-  @@after_eachs = []
-  @@before_all = []
-  @@after_all = []
-  @@passed_tests = 0
-  @@failed_tests = 0
-  @@response = []
-  class << self
-    def before_each(lambda)
-      @@before_eachs.append(lambda)
-    end
+$passed_tests = 0
+$failed_tests = 0
+$response = []
 
-    def after_each(lambda)
-      @@after_eachs.append(lambda)
-    end
-
-    def before_all(lambda)
-      @@before_all.append(lambda)
-    end
-
-    def after_all(lambda)
-      @@after_all.append(lambda)
-    end
-
-    def it(_describe)
-      @@total_tests += 1
-      @@before_eachs.map(&:call)
-      p yield
-      @@after_eachs.map(&:call)
-    end
-
-    def to_be(expected, value)
-      if value.eql? expected
-        p @@response << { name: message(value, expected), status: true }
-        @@passed_tests += 1
-      else
-        p @@response << { name: message(value, expected), status: false }
-        @@failed_tests += 1
+module Test
+  class TestFramework
+    @@total_tests = 0
+    @@before_eachs = []
+    @@after_eachs = []
+    @@before_all = []
+    @@after_all = []
+    class << self
+      def before_each(lambda)
+        @@before_eachs.append(lambda)
       end
-    end
 
-    def response
-      "#{@@passed_tests}, #{@@failed_tests}, #{@@response}"
-    end
+      def after_each(lambda)
+        @@after_eachs.append(lambda)
+      end
 
-    private
+      def before_all(lambda)
+        @@before_all.append(lambda)
+      end
 
-    def message(value, expect)
-      "expect #{value} to have the same object_id as #{expect}"
+      def after_all(lambda)
+        @@after_all.append(lambda)
+      end
+
+      def it(_describe)
+        @@total_tests += 1
+        @@before_eachs.map(&:call)
+        p yield
+        @@after_eachs.map(&:call)
+      end
+
+      def describe(_describe)
+        @@before_all.map(&:call)
+        yield
+        @@after_all.map(&:call)
+      end
+
+      def response
+        "#{@@passed_tests}, #{@@failed_tests}, #{@@response}"
+      end
+
+      def show_result
+        puts "
+          TotalTest: #{@@total_tests}
+          Test Suites: passed, total
+          Tests: #{$passed_tests} passed, #{$total_tests} total
+          "
+      end
     end
   end
 end
