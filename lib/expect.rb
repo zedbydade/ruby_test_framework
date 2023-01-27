ad708dcd02e33ee1b4bbb88afa40920037a1ec3f
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-01-26 22:52:34 -0300

    Feat: Add it method

--- lib/expect.rb
+++ lib/expect.rb
@@ -1,24 +1,2 @@
 module Expect
-  @@passed_tests = 0
-  @@failed_tests = 0
-  @@response = []
-  def to_be(expected, value)
-    if value.eql? expected
-      @@response << { name: message(value, expected), status: true }
-      @@passed_tests += 1
-    else
-      @@response << { name: message(value, expected), status: false }
-      @@failed_tests += 1
-    end
-  end
-
-  def response
-    "#{@@passed_tests}, #{@@failed_tests}, #{@@response}"
-  end
-
-  private
-
-  def message(value, expect)
-    "expect #{value} to have the same object_id as #{expect}"
-  end
 end
--- lib/test_framework.rb
+++ lib/test_framework.rb
@@ -1,19 +1,54 @@
-VERSION = '6.1.0'
+class TestFramework
+  @@total_tests = 0
+  @@before_eachs = []
+  @@after_eachs = []
+  @@before_all = []
+  @@after_all = []
+  @@passed_tests = 0
+  @@failed_tests = 0
+  @@response = []
+  class << self
+    def before_each(lambda)
+      @@before_eachs.append(lambda)
+    end
 
-class TestFrameworkCore
-  def beforeEach
-    yield
-  end
+    def after_each(lambda)
+      @@after_eachs.append(lambda)
+    end
 
-  def afterEach
-    yield
-  end
+    def before_all(lambda)
+      @@before_all.append(lambda)
+    end
 
-  def beforeAll
-    yield
-  end
+    def after_all(lambda)
+      @@after_all.append(lambda)
+    end
 
-  def afterAll
-    yield
+    def it(_describe)
+      @@total_tests += 1
+      @@before_eachs.map(&:call)
+      p yield
+      @@after_eachs.map(&:call)
+    end
+
+    def to_be(expected, value)
+      if value.eql? expected
+        p @@response << { name: message(value, expected), status: true }
+        @@passed_tests += 1
+      else
+        p @@response << { name: message(value, expected), status: false }
+        @@failed_tests += 1
+      end
+    end
+
+    def response
+      "#{@@passed_tests}, #{@@failed_tests}, #{@@response}"
+    end
+
+    private
+
+    def message(value, expect)
+      "expect #{value} to have the same object_id as #{expect}"
+    end
   end
 end
