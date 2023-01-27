2c35dea9291d8a4ab7036d1e625011422e07cdfa
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-01-27 20:58:28 -0300

    Add to_be method

--- lib/expect.rb
+++ lib/expect.rb
@@ -1,2 +1,17 @@
 module Expect
+  def to_be(expected, value)
+    if value.eql? expected
+      p $response << { name: message(value, expected), status: true }
+      $passed_tests += 1
+    else
+      p $response << { name: message(value, expected), status: false }
+      $failed_tests += 1
+    end
+  end
+
+  private
+
+  def message(value, expect)
+    "expect #{value} to have the same object_id as #{expect}"
+  end
 end
